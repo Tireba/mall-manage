d33fdf015b669c43b12645fcf04f05bfd4ea6d84
Tireba <1131391789@qq.com>
2022-03-22 23:31:22 +0800

    项目初始化

---
+++ mall-system/src/main/java/com/tireba/mallsystem/MallSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tireba.mallsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSystemApplication.class, args);
+    }
+
+}
---
+++ mall-system/src/main/java/com/tireba/mallsystem/utils/CodeAutoGeneratorUtils.java
@@ -0,0 +1,42 @@
+package com.tireba.mallsystem.utils;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.fill.Column;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Description 代码生成器
+ * @Date 2022/3/19 23:20
+ * @Create by Tireba
+ */
+
+public class CodeAutoGeneratorUtils {
+
+    public static void main(String[] args) {
+        // 交互式
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/mall_manage","root","Tireba872..")
+                // 全局配置
+                .globalConfig((scanner, builder) -> builder.author(scanner.apply("请输入作者名称？")).fileOverride())
+                // 包配置
+                .packageConfig((scanner, builder) -> builder.parent(scanner.apply("请输入包名？")))
+                // 策略配置
+                .strategyConfig((scanner, builder) -> builder.addInclude(getTables(scanner.apply("请输入表名，多个英文逗号分隔？所有输入 all")))
+                        .controllerBuilder().enableRestStyle().enableHyphenStyle()
+                        .entityBuilder().enableLombok().addTableFills(
+                                new Column("create_time", FieldFill.INSERT)
+                        ).build())
+                   .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+
+    // 处理 all 情况
+    protected static List<String> getTables(String tables) {
+        return "all".equals(tables) ? Collections.emptyList() : Arrays.asList(tables.split(","));
+    }
+
+}
